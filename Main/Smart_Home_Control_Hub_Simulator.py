7f4cc7b1b8e6f78aa230a02f27666eee91d8ebd9
Visshwa_Elayaraja <visshwainfo@gmail.com>
2026-07-10 16:59:05 +0530

    Finished the base code for Simulator functioning.

--- Main/Smart_Home_Control_Hub_Simulator.py
+++ Main/Smart_Home_Control_Hub_Simulator.py
@@ -13,6 +13,7 @@ class SmartDevice:
         self.id = id
         self.is_on = False
         self.idle_ticks = 0
+        self.maximum_idle_time = 20
 
     def Log_Activity(self, log):
         print(log)
@@ -26,10 +27,15 @@ class SmartDevice:
     def increase_idle_ticks(self):
         self.idle_ticks += 1
 
-    def motion_automation(self, sensor_pack):
+    def automated_motion_detection(self, sensor_pack):
         if sensor_pack.motion_detected:
             self.turn_on()
             self.Log_Activity(f"[{self.name.upper()} TURNED ON]: Due to room occupancy.")
+        else:
+            self.idle_ticks += 1
+            if self.idle_ticks == self.maximum_idle_time:
+                self.idle_ticks = 0
+                self.turn_off()
 
     def handle_power_outage(self, sensor_pack):
         # Default Rule: If the power source is switched to 'Battery', turn_off all appliances.
@@ -69,8 +75,8 @@ class AirConditioner(SmartDevice):
             self.Log_Activity("[BATTERY SAVER ON]: Due to power outage.")
 
     def handle_obstacles_and_requirements(self, sensor_pack):
-        self.motion_automation()
-        self.handle_power_outage()
+        self.automated_motion_detection(sensor_pack)
+        self.handle_power_outage(sensor_pack)
 
         if (sensor_pack.global_hazard == "Fire") or (sensor_pack.smoke_detected):
             self.turn_off()
@@ -105,8 +111,8 @@ class Television(SmartDevice):
         else: print("Channel unvailable.")
     
     def handle_obstacles_and_requirements(self, sensor_pack):
-        self.motion_automation()
-        self.handle_power_outage()
+        self.automated_motion_detection(sensor_pack)
+        self.handle_power_outage(sensor_pack)
 
         if (sensor_pack.global_hazard == "Fire"):
             self.turn_off()
@@ -140,8 +146,8 @@ class SmartFan(SmartDevice):
             self.maximum_speed = 2
     
     def handle_obstacles_and_requirements(self, sensor_pack):
-        self.motion_automation()
-        self.handle_power_outage()
+        self.automated_motion_detection(sensor_pack)
+        self.handle_power_outage(sensor_pack)
 
         if (sensor_pack.global_hazard == "Fire") or (sensor_pack.smoke_detected):
             self.turn_on()
@@ -198,11 +204,17 @@ class SimulatorEngine():
             for device in device_list:
                 device.handle_obstacles_and_requirements(current_system)
 
-    def start_engine(self):
-        
+    def start_engine(self, sleep_time):
+        run_time = 0
         while True:
             
-            time.sleep(1)
+            print(f"===== RUN TIME : {run_time} =====")  
+
+            self.generate_sensor_system()
+
+            self.manage_obstacles_and_requirements()
+
+            time.sleep(sleep_time)
 
 
 # ===========================================================================
@@ -211,5 +223,19 @@ class SimulatorEngine():
 # MAIN
 if __name__ == "__main__":
     
-    print("=== STARTING CONTROL HUB SIMULATOR ===")        
-    
+    Engine = SimulatorEngine()
+
+    while True:
+        try:
+            sleep_time = int(input("Enter sleep time for every iteration:   "))
+            break
+        except Exception as Error:
+            print(Error)
+
+    def Main():
+        Engine.start_engine()
+
+    print("=== STARTING CONTROL HUB SIMULATOR ===")
+    print(f"[   NOTE: EVERY {sleep_time} SECONDS IS COUNTED AS 1 SECOND IN THIS SIMULATOR   ]")     # Configurable sleep time for user preference and flexibility.
+
+    Main()
